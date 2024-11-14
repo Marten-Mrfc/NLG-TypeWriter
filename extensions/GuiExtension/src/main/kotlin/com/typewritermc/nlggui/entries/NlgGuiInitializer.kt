cfd4c090fb1b0557a1ad02353b0317c42b1bb640
Marten Mrfc <101009922+Marten-Mrfc@users.noreply.github.com>
2024-11-14 08:42:15 +0100

    [Extension] Added GuiListener + PBV

--- extensions/GuiExtension/src/main/kotlin/com/typewritermc/nlggui/entries/NlgGuiInitializer.kt
+++ extensions/GuiExtension/src/main/kotlin/com/typewritermc/nlggui/entries/NlgGuiInitializer.kt
@@ -1,16 +1,24 @@
 import com.typewritermc.core.extension.Initializable
 import com.typewritermc.core.extension.annotations.Initializer
+import com.typewritermc.engine.paper.plugin
 import com.typewritermc.nlggui.entries.commands
+import com.typewritermc.nlggui.entries.GuiListener
+import org.bukkit.Bukkit
+import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.plugin.PluginManager
 
 @Initializer
 object NlgGuiInitializer : Initializable {
+    private val listener = GuiListener()
+
     override fun initialize() {
         commands()
+        val pluginManager: PluginManager = Bukkit.getPluginManager()
+        pluginManager.registerEvents(listener, plugin)
         println("Initialized NlgGui")
     }
 
     override fun shutdown() {
-        // Do something when the extension is shutdown
+        InventoryClickEvent.getHandlerList().unregister(listener)
     }
-
 }
